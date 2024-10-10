434bfc4274a88f8cab5f81677e0a56594f5534a9
wangqian <wangqian.xd@haier.com>
2024-10-10 11:20:34 +0800

    fix: 适配windows环境

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: path.resolve(__dirname, './dist/'),
+  base: './',
   plugins: [
     vue(),
     Components({
